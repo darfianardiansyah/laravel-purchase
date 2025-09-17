2a508d8bc0cb87bd283ffcb210349d4467460e6f
darfianardiansyah <darfianardiansyah@gmail.com>
2025-09-17 10:11:45 +0700

    Add : Purchases index

--- app/Http/Controllers/Admin/PurchaseController.php
+++ app/Http/Controllers/Admin/PurchaseController.php
@@ -7,6 +7,10 @@ use Illuminate\Http\Request;
 
 class PurchaseController extends Controller
 {
+    public function index()
+    {
+        return view('admin.purchases.index');
+    }
     public function report()
     {
         return view('admin.purchases.report');
